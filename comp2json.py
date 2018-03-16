d934d02be6c07094eb030ce774b30fc16d701f69
Tee <i@lado.me>
2018-03-16 23:02:26 +0800

    component to json script for import multiple component

---
+++ comp2json.py
@@ -0,0 +1,30 @@
+import os
+import json
+import glob
+
+all_files = glob.glob('./pot/*.pot')
+
+def comp2json():
+    result = []
+
+    for f in all_files:
+        fn = os.path.splitext(os.path.basename(f))[0]
+        fn_thin = fn[1:-1].lower()
+
+        result.append({
+            "name": fn,
+            "slug": fn_thin,
+            "repo": "https://github.com/wastee/REAPER_zh-hans.git",
+            "push": "git@github.com:wastee/REAPER_zh-hans.git",
+            "branch": "master",
+            "filemask": "po/{}-*.po".format(fn),
+            "new_base": "pot/{}.pot".format(fn),
+            "committer_name": "REAPER Weblate",
+            "committer_email": "i@lado.me"
+        })
+
+    with open('./import_components.json', 'w+', encoding='utf-8') as f:
+        json.dump(result, f)
+
+if __name__ == '__main__':
+    comp2json()
