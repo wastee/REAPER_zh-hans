9baf8cfe9abf7a64ac10330354bd466b570010b6
Tee <i@lado.me>
2018-03-21 20:54:43 +0800

    update c2j script

--- comp2json.py
+++ comp2json.py
@@ -9,20 +9,20 @@ def comp2json():
 
     for f in all_files:
         fn = os.path.splitext(os.path.basename(f))[0]
-        fn_thin = fn[1:-1].lower()
-
-        result.append({
-            "name": fn,
-            "slug": fn_thin,
-            # "repo": "https://github.com/wastee/REAPER_zh-hans.git",
-            "repo": "weblate://reaper/about",
-            "push": "git@github.com:wastee/REAPER_zh-hans.git",
-            "branch": "master",
-            "filemask": "po/{}-*.po".format(fn),
-            "new_base": "pot/{}.pot".format(fn),
-            "committer_name": "REAPER Weblate",
-            "committer_email": "i@lado.me"
-        })
+        fn_thin = fn.lower()
+        if fn_thin != 'about':
+            result.append({
+                "name": fn,
+                "slug": fn_thin,
+                # "repo": "https://github.com/wastee/REAPER_zh-hans.git",
+                "repo": "weblate://reaper/about",
+                # "push": "git@github.com:wastee/REAPER_zh-hans.git",
+                "branch": "master",
+                "filemask": "po/{}-*.po".format(fn),
+                "new_base": "pot/{}.pot".format(fn),
+                "committer_name": "REAPER Weblate",
+                "committer_email": "i@lado.me"
+            })
 
     with open('./import_components.json', 'w+', encoding='utf-8') as f:
         json.dump(result, f)
